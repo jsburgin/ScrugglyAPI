1881d5d8d432bd51b4cdfd82e217bda3e824d96d
Joshua Burgin <jsburgin@crimson.ua.edu>
2017-10-11 21:58:44 -0400

    added base express app

---
+++ src/utils/setup.js
@@ -0,0 +1,37 @@
+const bodyParser = require('body-parser');
+const errorHandler = require('../middleware/errors');
+const configRoutes = require('../routes/config');
+
+const port = process.env.PORT || '3000';
+
+/**
+ * initial app configuration
+ * @param  {Object} app express application
+ */
+const configure = (app) => {
+  app.set('port', port);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  
+  configRoutes(app);
+  app.use(errorHandler);
+};
+
+/**
+ * app startup
+ * @param  {Object} server
+ */
+const start = (server) => {
+  server.listen(port);
+
+  server.on('listening', () => {});
+
+  server.on('error', (err) => {
+    throw err;
+  });
+};
+
+module.exports = {
+  configure,
+  start,
+};
