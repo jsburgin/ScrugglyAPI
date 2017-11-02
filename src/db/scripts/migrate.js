3e2fc283f1f0f37a9d25585d3d15260dbf236903
Sidney Packer <sidp@knights.ucf.edu>
2017-11-01 22:16:24 -0400

    Change migration script to handle multiple files

--- src/db/scripts/migrate.js
+++ src/db/scripts/migrate.js
@@ -2,23 +2,32 @@ const fs = require('fs');
 const { exec } = require('child_process');
 
 const migrateCommands = {
-  up: '0',
-  down: '1',
+  do: 0,
+  undo: 1,
 };
 
-if (process.argv[2] === undefined || !migrateCommands[process.argv[2]]) {
-  throw new Error(`Proper migration command not found. Available commands: ${Object.keys(migrateCommands)}`);
+if (process.argv[2] === undefined || migrateCommands[process.argv[2]] === undefined) {
+  throw new Error(`Migration command '${process.argv[2]}' not found. Available commands: ${Object.keys(migrateCommands)}`);
 }
 
-const files = fs.readdirSync('./src/db/schema');
-const fileToExecute = files.slice(-2)[migrateCommands[process.argv[2]]];
+let files = fs.readdirSync('./src/db/schema');
 
-exec(`psql -f src/db/schema/${fileToExecute}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error('\x1b[31m', `exec error: ${error}`);
-    return;
+// Undo in reverse.
+if (migrateCommands[process.argv[2]] === 1) {
+  files = files.reverse();
+}
+
+files.reverse().map((file, i) => {
+  if (i % 2 === migrateCommands[process.argv[2]]) {
+    exec(`psql -f src/db/schema/${file}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error('\x1b[31m', `exec error: ${error}`);
+        return;
+      }
+      console.log('\x1b[32m', `${stdout}`);
+      console.log('\x1b[33m', `${stderr}`);
+    });
   }
-  console.log('\x1b[32m', `${stdout}`);
-  console.log('\x1b[33m', `${stderr}`);
+  return true;
 });
 
